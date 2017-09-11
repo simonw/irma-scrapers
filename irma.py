a030df83eebb6d06270a522f666df6e12b6ea99f
Simon Willison <simonw@eventbrite.com>
2017-09-11 07:43:36 -0700

    Scrapers for Georgia shelter data, closes #2
    Web interface: https://gema-soc.maps.arcgis.com/apps/webappviewer/index.html?id=279ef7cfc1da45edb640723c12b02b18
    
    Two new data sources:
    
    * Active shelters: https://services1.arcgis.com/2iUE8l8JKrP2tygQ/arcgis/rest/services/SheltersActive/FeatureServer/0/query?f=json&where=shelter_information_shelter_type%20%3C%3E%20%27Reception%20Care%20Ctr.%27&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=*&outSR=102100&resultOffset=0&resultRecordCount=1000
    * Animal shelters: https://services1.arcgis.com/2iUE8l8JKrP2tygQ/arcgis/rest/services/AnimalShelters/FeatureServer/0/query?f=json&where=status%20%3D%20%27OPEN%27&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=*&outSR=102100&resultOffset=0&resultRecordCount=1000
    
    Example commits:
    
    * https://github.com/simonw/irma-scraped-data/commit/5e1c655e91a91018a2d5a213a737b80f817f4b34
    * https://github.com/simonw/irma-scraped-data/commit/d0997b356872672203c5ecf872b4810073dd51be

--- irma.py
+++ irma.py
@@ -11,20 +11,40 @@ import time
 import json
 
 
+def objectid(d):
+    # Different datasets represent objectid in different ways
+    return d.get('OBJECTID') or d['ObjectID']
+
+
+def shelter_name(d):
+    return d.get('SHELTER_NAME') or d['label']
+
+
+def shelter_county(d):
+    return d.get('COUNTY_PARISH') or d['county']
+
+
 class FemaOpenShelters(BaseScraper):
     filepath = 'fema-open-shelters.json'
     url = 'https://gis.fema.gov/REST/services/NSS/OpenShelters/MapServer/0/query?f=json&returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry=%7B%22xmin%22%3A-10018754.171396945%2C%22ymin%22%3A2504688.5428529754%2C%22xmax%22%3A-7514065.628548954%2C%22ymax%22%3A5009377.085700965%2C%22spatialReference%22%3A%7B%22wkid%22%3A102100%7D%7D&geometryType=esriGeometryEnvelope&inSR=102100&outFields=*&outSR=102100'
+    source_url = None
 
-    def update_message(self, old_data, new_data):
-        new_objects = [o for o in new_data if not any(o2 for o2 in old_data if o2['OBJECTID'] == o['OBJECTID'])]
-        removed_objects = [o for o in old_data if not any(o2 for o2 in new_data if o2['OBJECTID'] == o['OBJECTID'])]
+    def create_message(self, new_data):
+        return self.update_message([], new_data, verb='Created')
+
+    def update_message(self, old_data, new_data, verb='Updated'):
+        new_objects = [o for o in new_data if not any(o2 for o2 in old_data if objectid(o2) == objectid(o))]
+        removed_objects = [o for o in old_data if not any(o2 for o2 in new_data if objectid(o2) == objectid(o))]
         message = []
 
         def name(row):
-            if 'COUNTY_PARISH' in row:
-                return '%s (%s County)' % (row['SHELTER_NAME'], row['COUNTY_PARISH'].title())
+            if 'COUNTY_PARISH' in row or 'county' in row:
+                s = '%s (%s County)' % (shelter_name(row), shelter_county(row).title())
+            elif 'CITY' in row and 'STATE' in row:
+                s = '%s (%s, %s)' % (shelter_name(row), row['CITY'].title(), row['STATE'])
             else:
-                return '%s (%s, %s)' % (row['SHELTER_NAME'], row['CITY'].title(), row['STATE'])
+                s = shelter_name(row)
+            return s.replace('County County', 'County')
 
         for new_object in new_objects:
             message.append('Added shelter: %s' % name(new_object))
@@ -36,7 +56,7 @@ class FemaOpenShelters(BaseScraper):
             message.append('')
         num_updated = 0
         for new_object in new_data:
-            old_object = [o for o in old_data if o['OBJECTID'] == new_object['OBJECTID']]
+            old_object = [o for o in old_data if objectid(o) == objectid(new_object)]
             if not old_object:
                 continue
             old_object = old_object[0]
@@ -60,19 +80,33 @@ class FemaOpenShelters(BaseScraper):
         if summary:
             summary_text = self.filepath + ': ' + (', '.join(summary))
         else:
-            summary_text = 'Updated %s' % self.filepath
+            summary_text = '%s %s' % (verb, self.filepath)
+        if self.source_url:
+            body += '\nChange detected on %s' % self.source_url
         return summary_text + '\n\n' + body
 
     def fetch_data(self):
         data = requests.get(self.url).json()
         shelters = [feature['attributes'] for feature in data['features']]
-        shelters.sort(key=lambda s: s['OBJECTID'])
+        shelters.sort(key=lambda s: objectid(s))
         return shelters
 
 
 class FemaNSS(FemaOpenShelters):
     filepath = 'fema-nss.json'
     url = 'https://gis.fema.gov/REST/services/NSS/FEMA_NSS/MapServer/0/query?f=json&returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry=%7B%22xmin%22%3A-10018754.171396945%2C%22ymin%22%3A2504688.5428529754%2C%22xmax%22%3A-7514065.628548954%2C%22ymax%22%3A5009377.085700965%2C%22spatialReference%22%3A%7B%22wkid%22%3A102100%7D%7D&geometryType=esriGeometryEnvelope&inSR=102100&outFields=*&outSR=102100'
+
+
+class GemaAnimalShelters(FemaOpenShelters):
+    filepath = 'georgia-gema-animal-shelters.json'
+    url = 'https://services1.arcgis.com/2iUE8l8JKrP2tygQ/arcgis/rest/services/AnimalShelters/FeatureServer/0/query?f=json&where=status%20%3D%20%27OPEN%27&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=*&outSR=102100&resultOffset=0&resultRecordCount=1000'
+    source_url = 'https://gema-soc.maps.arcgis.com/apps/webappviewer/index.html?id=279ef7cfc1da45edb640723c12b02b18'
+
+
+class GemaActiveShelters(FemaOpenShelters):
+    filepath = 'georgia-gema-active-shelters.json'
+    url = 'https://services1.arcgis.com/2iUE8l8JKrP2tygQ/arcgis/rest/services/SheltersActive/FeatureServer/0/query?f=json&where=shelter_information_shelter_type%20%3C%3E%20%27Reception%20Care%20Ctr.%27&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=*&outSR=102100&resultOffset=0&resultRecordCount=1000'
+    source_url = 'https://gema-soc.maps.arcgis.com/apps/webappviewer/index.html?id=279ef7cfc1da45edb640723c12b02b18'
 
 
 class ZeemapsScraper(BaseScraper):
@@ -132,7 +166,7 @@ class PascoCounty(BaseScraper):
             self.filepath,
             verb=verb
         )
-        message += '\n\nChange detected on http://www.pascocountyfl.net/index.aspx?NID=2816'
+        message += '\nChange detected on http://www.pascocountyfl.net/index.aspx?NID=2816'
         return message
 
     def fetch_data(self):
@@ -185,7 +219,7 @@ class LedgerPolkCounty(BaseScraper):
             )
         else:
             summary_text = '%s %s' % (verb, self.filepath)
-        return '%s\n\n%s\n\nChange detected on %s' % (
+        return '%s\n\n%s\nChange detected on %s' % (
             summary_text, body, self.url
         )
 
@@ -251,7 +285,7 @@ class HernandoCountyShelters(BaseScraper):
             )
         else:
             summary_text = '%s %s' % (verb, self.filepath)
-        return '%s\n\n%s\n\nChange detected on %s' % (
+        return '%s\n\n%s\nChange detected on %s' % (
             summary_text, body, self.url
         )
 
@@ -327,7 +361,7 @@ class FloridaDisasterShelters(BaseScraper):
         current_names = [name(n) for n in new_data]
         previous_names = [name(n) for n in old_data]
         message = update_message_from_names(current_names, previous_names, self.filepath)
-        message += '\n\nChange detected on %s' % self.url
+        message += '\nChange detected on %s' % self.url
         return message
 
     def fetch_data(self):
@@ -379,18 +413,21 @@ if __name__ == '__main__':
     scrapers = [
         klass(github_token, slack_token)
         for klass in (
-            FemaOpenShelters,
-            FemaNSS,
-            IrmaShelters,
-            IrmaShelterDupes,
-            FloridaDisasterShelters,
-            ZeemapsScraper,
-            PascoCounty,
-            CrowdSourceRescue,
-            LedgerPolkCounty,
-            HernandoCountyShelters,
-            FplStormOutages,
-            FplCountyOutages,
+            #FemaOpenShelters,
+            #FemaNSS,
+            #IrmaShelters,
+            #IrmaShelterDupes,
+            #FloridaDisasterShelters,
+            #ZeemapsScraper,
+            #PascoCounty,
+            #CrowdSourceRescue,
+            #LedgerPolkCounty,
+            #HernandoCountyShelters,
+            #FplStormOutages,
+            #FplCountyOutages,
+            GemaAnimalShelters,
+            GemaActiveShelters,
+
         )
     ]
     while True:
