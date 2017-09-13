37a8a42eae3303282b03834321d105d41117de46
mjturtora <mjturtora@gmail.com>
2017-09-13 13:45:50 -0400

    read_irma_shelters.py writes api and scraped data to csv

--- read_irma_shelters.py
+++ read_irma_shelters.py
@@ -1,15 +1,30 @@
 #https://eureka.ykyuen.info/2015/04/02/python-read-and-parse-a-json-via-url/
-import urllib2
-import json
+import requests
+import pandas as pd
 
-#response = urllib2.urlopen("https://github.com/simonw/irma-scraped-data/blob/master/irma-shelters.json")
-#data = json.loads(response)
-#print data
 
-# https://gist.github.com/sirleech/2660189
-req = urllib2.Request("https://github.com/simonw/irma-scraped-data/blob/master/irma-shelters.json")
-opener = urllib2.build_opener()
-f = opener.open(req)
-json = json.loads(f.read())
-print json
+def fetch_json(url):
+    return requests.get(url).json()
 
+
+def fetch_api_data(url):
+    data = fetch_json(url)
+    j_data = data['shelters']
+    return pd.DataFrame(j_data)
+
+
+def fetch_scraped_data(url):
+    j_data = fetch_json(url)
+    return pd.DataFrame(j_data)
+
+
+if __name__ == '__main__':
+    url = 'https://irma-api.herokuapp.com/api/v1/shelters'
+    df_api_shelters = fetch_api_data(url)
+    print('irma-api shape = ', df_api_shelters.shape)
+    df_api_shelters.to_csv('api.csv', encoding='utf-8')
+
+    url = 'https://raw.githubusercontent.com/simonw/irma-scraped-data/master/irma-shelters.json'
+    df_scraped_shelters = fetch_scraped_data(url)
+    print('irma-scraped shape = ', df_scraped_shelters.shape)
+    df_scraped_shelters.to_csv('scraped.csv', encoding='utf-8')
