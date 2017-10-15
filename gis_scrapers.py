ff870e1e9b0a2de929c60e110d5e8d156343d2a5
Simon Willison <simonw@eventbrite.com>
2017-10-15 16:07:40 -0700

    Updated FemaNSS to grab (hopefully) all of USA
    This should get us Puerto Rico and California (Sonoma) shelters.
    
    I had to figure out the projections used for the bounding boxes, which was really hard. This ended up working:
    
        from pyproj import Proj, transform
        from_projection = Proj(init='epsg:3857')
        to_projection = Proj(proj='latlong', ellps='WGS84', datum='WGS84')
        transform(to_projection, from_projection, -129.4, -0.5)
        # (-14404742.108649602, -55660.4518654215)
        transform(to_projection, from_projection, -53.8, 51.9)
        # (-5988988.6046781195, 6782064.328749425)

--- gis_scrapers.py
+++ gis_scrapers.py
@@ -88,7 +88,7 @@ class FemaOpenShelters(BaseGisScraper):
 
 class FemaNSS(BaseGisScraper):
     filepath = 'fema-nss.json'
-    url = 'https://gis.fema.gov/REST/services/NSS/FEMA_NSS/MapServer/0/query?f=json&returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry=%7B%22xmin%22%3A-10018754.171396945%2C%22ymin%22%3A2504688.5428529754%2C%22xmax%22%3A-7514065.628548954%2C%22ymax%22%3A5009377.085700965%2C%22spatialReference%22%3A%7B%22wkid%22%3A102100%7D%7D&geometryType=esriGeometryEnvelope&inSR=102100&outFields=*&outSR=102100'
+    url = 'https://gis.fema.gov/REST/services/NSS/FEMA_NSS/MapServer/0/query?f=json&returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry=%7B%22xmin%22%3A+-14404742.108649602%2C+%22ymin%22%3A+-55660.4518654215%2C+%22ymax%22%3A+6782064.328749425%2C+%22xmax%22%3A+-5988988.6046781195%2C+%22spatialReference%22%3A+%7B%22wkid%22%3A+102100%7D%7D&geometryType=esriGeometryEnvelope&inSR=102100&outFields=*&outSR=102100'
 
 
 class GemaAnimalShelters(BaseGisScraper):
